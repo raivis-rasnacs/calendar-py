f74a96e2f4ad89ca53d8de42a2c44157f3540733
raivisss <raivis_868@inbox.lv>
2022-11-15 01:13:36 +0200

    v1

---
+++ my_calendar.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+def vaiGaraisGads(gads):
+    if gads % 4 == 0:
+        if gads % 100 == 0:
+            if gads % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+def dienuSkaits(menesis, garaisGads = False):
+    if int(menesis) == 2:
+        if garaisGads is True:
+            return 29
+        else:
+            return 28
+    elif int(menesis) in [4, 6, 9, 11]:
+        return 30
+    else:
+        return 31
+
+def ievaddatuParbaude(datums):
+    if len(datums) == 7:
+        if datums[2] == "/":
+            if datums[:2].isdigit() and datums[3:].isdigit():
+                return True
+    return False
+
+def kalendars(dienuSkaits, menesis, gads):
+    print("Pr Ot Tr Ce Pk Se Sv")  # dienu nosaukumi
+    pirmaMenesaDiena = int(datetime(int(gads), int(menesis), 1).weekday() + 1)  # mēneša pirmā diena
+    diena = 1  # iestata skaitītāju dienām
+    nedelasDiena = 1  # iestata skaitītāju nedēļas dienām
+
+    print("   " * (pirmaMenesaDiena - 1), end="")  # izdrukā tukšās dienas mēneša sākumā
+    nedelasDiena += pirmaMenesaDiena - 1  # uzsāk skaitīt pirmās nedēļas dienas
+        
+    while diena <= dienuSkaits:  # kamēr nav izvadītas visas mēneša dienas
+        if nedelasDiena % 7 == 1:  # ja beigusies nedēļa
+            print("")  # pāriet jaunā rindā
+            nedelasDiena = 1  # atiestata nedēļas dienu skaitītāju
+        
+        if diena < 10:
+            print(f"{diena}  ", end="")  # drukā viencipara skaitli
+        else:
+            print(f"{diena} ", end="")  # drukā divciparu skaitli
+
+        diena += 1  # skaita dienas
+        nedelasDiena += 1  # skaita nedēļas dienas
+
+""" Ievads """
+datums = input("Datums (mm/gggg): ")
+
+if ievaddatuParbaude(datums) is True:
+    menesis = int(datums[:2])  # atdala mēnesi
+    gads = int(datums[3:])  # atdala gadu
+
+    dienas = dienuSkaits(menesis, vaiGaraisGads(gads))  # nosaka dienu skaitu
+    kalendars(dienas, menesis, gads)  # drukā kalendāru
+else:
+    print("Ievaddati nederīgi!")
+    
---
+++ test_calendar.py
@@ -0,0 +1,61 @@
+from my_calendar import vaiGaraisGads, dienuSkaits, ievaddatuParbaude
+
+
+""" Garais gads - testi """
+
+def test_vaiGG_2018():
+    assert vaiGaraisGads(2018) is False
+
+def test_vaiGG_2002():
+    assert vaiGaraisGads(2002) is False
+
+def test_vaiGG_1900():
+    assert vaiGaraisGads(1900) is False
+
+def test_vaiGG_1988():
+    assert vaiGaraisGads(1988) is True
+
+def test_vaiGG_2000():
+    assert vaiGaraisGads(2000) is True
+
+def test_vaiGG_2400():
+    assert vaiGaraisGads(2400) is True
+    
+
+""" Dienu skaits - testi """
+
+def test_dienuSkaits_feb28():
+    assert dienuSkaits(2, False) == 28
+
+def test_dienuSkaits_feb29():
+    assert dienuSkaits(2, True) == 29
+
+def test_dienuSkaits_30():
+    assert dienuSkaits(4) == 30  # aprīlis
+    assert dienuSkaits(6) == 30  # jūnijs
+    assert dienuSkaits(9) == 30  # septembris
+    assert dienuSkaits(11) == 30  # novembris
+
+def test_dienuSkaits_31():
+    assert dienuSkaits(1) == 31
+    assert dienuSkaits(3) == 31
+    assert dienuSkaits(5) == 31
+    assert dienuSkaits(7) == 31
+    assert dienuSkaits(8) == 31
+    assert dienuSkaits(10) == 31
+    assert dienuSkaits(12) == 31
+
+
+""" Ievaddatu pārbaude - testi """
+
+def test_ievaddatuParbaude_pareizi():
+    assert ievaddatuParbaude("11/2005") is True
+    assert ievaddatuParbaude("09/1878") is True
+    assert ievaddatuParbaude("01/2020") is True
+
+def test_ievaddatuParbaude_kludaini():
+    assert ievaddatuParbaude("1/1999") is False
+    assert ievaddatuParbaude("10.1999") is False
+    assert ievaddatuParbaude("04/800") is False
+    assert ievaddatuParbaude("12\1996") is False
+    assert ievaddatuParbaude("janvaris\1996") is False
